8adc04f523a7ec5a03180be661b83e4bca869c04
LEKRINGY <bigfoozah@gmail.com>
2016-08-19 11:47:35 +0300

    Version 1.9

--- Exemple.php
+++ Exemple.php
@@ -11,7 +11,5 @@ class Exemple
     private $nom;
     private $age;
     private $log="Tsy mpôka tsun";
-	private $log="T^^Log Branche code_php ";
-	
-	
+	private $log_code="Log Branche code_php ";
 }
